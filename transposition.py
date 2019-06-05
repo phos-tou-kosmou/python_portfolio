62d634c6f79119192dd5483e4d06bf5c2cccfd89
phos-tou-kosmou <kirklincoln1@gmail.com>
2019-06-04 22:55:39 -0400

    adding multithreaded feature for transpose for large matrices (i.e. 100x100, 1000x1000).  Contemplating using a theoretical implementaion for transposing a non-square matrix

--- transposition.py
+++ transposition.py
@@ -1,93 +1,115 @@
 #!/bin/python3
 import numpy as np
+from multiprocessing import Pool
 
-class Matrix: 
-   
-    Array = np.ndarray
-    x: Array = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
-    y: Array = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+x = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+y = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+
+print(x)
 
 
-    '''TODO: I would like to munge the input so if x were a 4x4 np.ndarray 
-    and y was an array of length 16, then I would force y into an np.array.  
-    Also an option return a flat array would be nice, depending on what the user is doing''' 
-    def __init__(self, x, y)
-        if(type(x) is np.ndarray and type(y) is np.ndarray):
-            self.x: Array = np.array(x)
-            self.y: Array = np.array(y) 
+def transpose(x: np.ndarray) -> np.ndarray:
+    def worker(item):
+        try:
+            transpose_arithmetic(x)
+        except:
+            print('error with array')
 
-    def transpose(x: np.ndarray) -> np.ndarray:
-        length: int = len(x) 
-        if(length == len(x[0])): 
-            
-            array_as_list: Array = create_list(x)
-            
-            newList: Array = np.arange(length*length)
-            print(type(newList))
-            k = 0
 
-            for i, val in enumerate(array_as_list):
-                if i == (length*length-1): break
-                newList[k] = array_as_list[i]
-                k = (k + length) % (length*length - 1)
+    pool_size = 5
 
-            newList[len(newList)-1] = array_as_list[length*length-1]
-            thing: Array = newList.reshape(length, length)
-            print(thing) 
+    pool = Pool(pool_size)
+     
+    array_as_list: np.ndarray = create_list(x)
 
-            return thing
+    for x in enumerate(array_as_list):
+        pool.apply_async(worker, (x, ))
+    pool.close()
+    pool.join()
+    return x
 
-    def add_arrays(x: Array, y: Array) -> Array:
-        length_x: int = len(x)
-        length_y: int = len(y)
-
-        if(length_x != length_y or len(x) != len(y)):
-            return "Warning: You can only add matrices with corresponding dimensions (i.e. 2x2 + 2x2, 3x2 + 3x2)"
+def transpose_arithmetic(x: np.ndarray) -> np.ndarray:
+    length: int = len(x) 
+    if(length == len(x[0])): 
         
-        newList: Array = np.arange(length_y*len(y))
+        array_as_list: np.ndarray = create_list(x)
         
-        array_y_list: Array = create_list(y)
-        array_x_list: Array = create_list(x)
+        newList: np.ndarray = np.arange(length*length)
+        k = 0
 
-        
-        #TODO: Implement generator that allows me to add each index and abstract for multiplication
-        for i, val in enumerate(array_y_list):
-            newList[i] = val + array_x_list[i]
-            
-        thing: Array = newList.reshape(length_y, len(y))
+        for i, val in enumerate(array_as_list):
+            if i == (length*length-1): break
+            newList[k] = array_as_list[i]
+            k = (k + length) % (length*length - 1)
+
+        newList[len(newList)-1] = array_as_list[length*length-1]
+        result: np.ndarray = newList.reshape(length, length)
         print(result)
-        
         return result
 
+def add_arrays(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    length_x: int = len(x)
+    length_y: int = len(y)
 
+    if(length_x != length_y or len(x) != len(y)):
+        return "Warning: You can only add matrices with corresponding dimensions (i.e. 2x2 + 2x2, 3x2 + 3x2)"
+    
+    newList: np.ndarray = np.arange(length_y*len(y))
+    
+    array_y_list: np.ndarray = create_list(y)
+    array_x_list: np.ndarray = create_list(x)
 
-    def multiply_arrays(x: Array, y: Array) -> Array:
-        length_x: int = len(x)
-        length_y: int = len(y)
-
-        newList: Array = np.arange(length_y*len(y))
-
-        array_y_list: Array = create_list(y)
-        array_x_list: Array = create_list(x)
-
-        j: int = 0    
+    
+    #TODO: Implement generator that allows me to add each index and abstract for multiplication
+    for i, val in enumerate(array_y_list):
+        newList[i] = val + array_x_list[i]
         
-        #TODO: Implement second for loop, which traverses each column using modulus arithmetic and orbit groups
-        for i, val in enumerate(array_y_list):
-            newList[i] = val + array_x_list[j]
+    result: np.ndarray = newList.reshape(length_y, len(y))
+    print(result)
+    
+    return result
+
+
+
+def multiply_matrices(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    length_x: int = len(x)
+    length_y: int = len(y)
+
+    newList: np.ndarray = np.arange(length_y*len(y))
+
+    array_y_list: np.ndarray = create_list(y)
+    array_x_list: np.ndarray = create_list(x)
+
+    j: int = 0    
+    
+    #TODO: Implement second for loop, which traverses each column using modulus arithmetic and orbit groups
+
+    k: int = 0 
+    
+    for i, val in enumerate(array_y_list):
+        while (k+1) != len(array_y_list):
+            newList[i] += val * array_x_list[j]
+            print(array_x_list[j])
             j = (j+len(x)) % (len(x)*len(x))
-            print(j)
-            
-        result: Array = newList.reshape(length_y, len(y))
-        print(result)
+            k = k + 1
+        k = 0
+        j = j + 1
+         
+    result: np.ndarray = newList.reshape(length_y, len(y))
+    print(result)
 
-        return result
+    return result
+
+def create_list(x: np.ndarray) -> list:
+    list_of_array = [w for y in x for w in y]
+    return list_of_array
+
+def main() -> np.ndarray:
+    multiply_matrices(x, y)
+
+transpose(x)
 
 
-
-    def create_list(x: np.ndarray) -> list:
-        list_of_array = [w for y in x for w in y]
-        return list_of_array
-
-def main(x: np.ndarray) -> np.ndarray:
-    multiply_arrays(x, y)
+#executor = concurrent.futures.ProcessPoolExecutor(10)
+#futures = [executor.submit(transpose, x) for x in num]
+#concurrent.futures.wait(futures)
