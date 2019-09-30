e4d92d0b3aee2d6669be48bcbc23ace42dee0f85
phos-tou-kosmou <51272102+phos-tou-kosmou@users.noreply.github.com>
2019-09-30 17:45:45 -0400

    Update transposition.py

--- linear_algebra/transposition.py
+++ linear_algebra/transposition.py
@@ -2,101 +2,98 @@
 import numpy as np
 from multiprocessing import Pool
 
-x = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
-y = np.array([[1, 2, 3, 4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+x = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+y = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
 
 
-
-def transpose_multi(x: np.ndarray) -> np.ndarray:
+def transpose_multi(arr: np.ndarray) -> np.ndarray:
     def worker(item):
         try:
-            transpose_arithmetic(x)
+            transpose(arr)
         except:
             print('error with array')
-
 
     pool_size = 5
 
     pool = Pool(pool_size)
-     
+
     # array_as_list: np.ndarray = create_list(x)
 
-    pool.apply_async(worker, (x, ))
+    pool.apply_async(worker, (x,))
     pool.close()
     pool.join()
     return x
 
-def transpose(x: np.ndarray) -> np.ndarray:
-    length: int = len(x) 
-    if(length == len(x[0])): 
-        
-        array_as_list: np.ndarray = create_list(x)
-        
-        newList: np.ndarray = np.zeros(length*length)
+
+def transpose(tx: np.ndarray) -> np.ndarray:
+    length: int = len(tx)
+    if length == len(tx[0]):
+
+        array_as_list: list = create_list(tx)
+
+        newList: np.ndarray = np.zeros(length * length)
         k = 0
 
         for i, val in enumerate(array_as_list):
-            if i == (length*length-1): break
+            if i == (length * length - 1): break
             newList[k] = array_as_list[i]
-            k = (k + length) % (length*length - 1)
+            k = (k + length) % (length * length - 1)
 
-        newList[len(newList)-1] = array_as_list[length*length-1]
+        newList[len(newList) - 1] = array_as_list[length * length - 1]
         result: np.ndarray = newList.reshape(length, length)
         return result
 
-def add_matrices(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    length_x: int = len(x)
-    length_y: int = len(y)
 
-    if(length_x != length_y or len(x) != len(y)):
+def add_matrices(amx: np.ndarray, amy: np.ndarray) -> np.ndarray:
+    length_x: int = len(amx)
+    length_y: int = len(amy)
+
+    if length_x != length_y or len(amx) != len(amy):
         return "Warning: You can only add matrices with corresponding dimensions (i.e. 2x2 + 2x2, 3x2 + 3x2)"
-    
-    newList: np.ndarray = np.zeros(length_y*len(y))
-    
-    array_y_list: np.ndarray = create_list(y)
-    array_x_list: np.ndarray = create_list(x)
 
-    
-    #TODO: Implement generator that allows me to add each index and abstract for multiplication
+    newList: np.ndarray = np.zeros(length_y * len(y))
+
+    array_y_list: list = create_list(y)
+    array_x_list: list = create_list(x)
+
+    # TODO: Implement generator that allows me to add each index and abstract for multiplication
     for i, val in enumerate(array_y_list):
         newList[i] = val + array_x_list[i]
-        
+
     result: np.ndarray = newList.reshape(length_y, len(y))
-    
+
     return result
 
 
+def multiply_matrices(mmx: np.ndarray, mmy: np.ndarray) -> np.ndarray:
+    lx: int = len(mmx)
+    length_y: int = len(mmy) - 1
 
-def multiply_matrices(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    lx: int = len(x)
-    length_y: int = len(y)-1
+    newList: np.ndarray = np.zeros((lx * lx,), dtype=np.int)
 
-    newList: np.ndarray = np.zeros((lx*lx,), dtype=np.int)
+    array_y: np.ndarray = mmy
+    array_x: np.ndarray = mmx
 
-    array_y: np.ndarray = y
-    array_x: np.ndarray = x
+    #  iterator = [(p) % (lx+i) for p in newList]
 
-  #  iterator = [(p) % (lx+i) for p in newList]
-
-  #  print(iterator)
-
-    j: int = 0    
+    #  print(iterator)
+    j: int = 0
     k: int = 0
     p: int = 0
-    
+
     for i in range(lx):
         for j in range(lx):
             while k < lx:
-
                 newList[p] += array_x[i][k] * array_y[k][j]
                 k = k + 1
 
             k = 0
             p = p + 1
-         
-    result: np.ndarray = newList.reshape(length_y+1, len(y))
+
+    result: np.ndarray = newList.reshape(length_y + 1, len(y))
 
     return result
+
 
 def determinant(x: np.ndarray) -> np.ndarray:
     '''I have this method here merely to think about
@@ -104,20 +101,20 @@ def determinant(x: np.ndarray) -> np.ndarray:
         QR algorithms, householder matrices, and hessenberg deflations
         mainly becuase determinants are not computed most of the time
         and there are real problems that arise after getting past a
-        4x4 matrix'''    
+        4x4 matrix'''
 
 
-# The return on this method might need to be np.ndarray as well
-# hard to tell with duck type 
-def create_list(x: np.ndarray) -> list:
-    list_of_array: np.ndarray = [w for y in x for w in y]
+def create_list(oldarr: np.ndarray) -> list:
+    list_of_array: list = [w for p in oldarr for w in p]
     return list_of_array
 
-print(transpose(x))
 
-#print(multiply_matrices(transpose_arithmetic(x), y))
+if __name__ == '__main__':
+    print(transpose_multi(x))
+
+# print(multiply_matrices(transpose_arithmetic(x), y))
 
 
-#executor = concurrent.futures.ProcessPoolExecutor(10)
-#futures = [executor.submit(transpose, x) for x in num]
-#concurrent.futures.wait(futures)
+# executor = concurrent.futures.ProcessPoolExecutor(10)
+# futures = [executor.submit(transpose, x) for x in num]
+# concurrent.futures.wait(futures)
